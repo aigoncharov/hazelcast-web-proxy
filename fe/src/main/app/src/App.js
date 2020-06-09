94a8adf7634f5460dc5e952d6d622cf6c1543cde
Neil Stevenson <neilstevenson@users.noreply.github.com>
2020-06-09 12:13:07 +0100

    Framework for front-end

---
+++ fe/src/main/app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>h1</h1>
+        <h2>h2</h2>
+        <h3>h3</h3>
+        <p>p</p>
+      </div>
+    )
+  }
+}
+
+export default App
